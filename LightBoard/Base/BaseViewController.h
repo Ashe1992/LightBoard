02ae934230a3fa4b6f5c10b79a37f842573f1dc2
deyijinfubing <1780555169@qq.com>
2018-04-27 15:03:41 +0800

    删除一些没用的东西

--- LightBoard/Base/BaseViewController.h
+++ LightBoard/Base/BaseViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  BaseViewController.h
 //  LightBoard
 //
 //  Created by DEJR on 2018/4/27.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface BaseViewController : UIViewController
 
 @end
-
