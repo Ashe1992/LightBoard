40ba3c68a33a9f6d56126158bc97e35f4ec2e832
deyijinfubing <1780555169@qq.com>
2018-04-27 13:39:12 +0800

    Initial Commit

---
+++ LightBoard/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LightBoard
+//
+//  Created by DEJR on 2018/4/27.
+//  Copyright © 2018年 bing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
